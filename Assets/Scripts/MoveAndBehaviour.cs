b013c9b2cabbf342b06f3a7f4746520ad420d8c8
상빈 <67950060+17642@users.noreply.github.com>
2024-03-18 21:01:33 +0900

    FILEADD
    ADD

---
+++ Assets/Scripts/MoveAndBehaviour.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MoveAndBehaviour:MonoBehaviour
+{
+
+    public void SelectMovement(string mode, Vector3 direction, int range = 1)
+    {
+
+    }
+
+    public void MoveToSelectedLocation(Vector3 direction)
+    {
+        
+    }
+
+    public bool IsMoveable(Vector3 direction)
+    {
+        return false;
+    }
+
+
+    public void AttackToSelectedLocation(Vector3 direction, int range)
+    {
+
+    }
+
+    public void UseSkill(object skill)
+    {
+
+    }
+
+    
+}
